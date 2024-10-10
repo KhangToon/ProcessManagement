4d20996ca14671cbf310a80f50bdf02693f505d5
Ton Tan Khang <87051539+KhangToon@users.noreply.github.com>
2024-10-10 15:38:49 +0700

    # 10/10/2024

---
+++ ProcessManagement/Models/PhieuXacDinhCD.cs
@@ -0,0 +1,12 @@
+﻿namespace ProcessManagement.Models
+{
+    public class PhieuXacDinhCD
+    {
+        public string TenSanPham { get; set; } = string.Empty;
+        public string MaSanPham { get; set; } = string.Empty;
+        public string MaQuanLyLOT { get; set; } = string.Empty;
+        public string MaKHSX { get; set; } = string.Empty;
+
+        public List<NVLmoiNguyenCong> DSNVLofNguyenCongs { get; set; } = new();
+    }
+}
