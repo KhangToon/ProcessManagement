15c1d1489cbf1489ba459c7b2684c924e99f32d8
Ton Tan Khang <87051539+KhangToon@users.noreply.github.com>
2024-11-21 15:23:38 +0700

    # QRIDLOT - Print Function

---
+++ ProcessManagement/Services/KHOServices.cs
@@ -0,0 +1,12 @@
+﻿using ProcessManagement.Services.SQLServer;
+using ProcessManagement.Models.KHO_NVL;
+using ProcessManagement.Models.KHO_NVL.NhapKho;
+using ProcessManagement.Models.KHO_NVL.XuatKho;
+
+namespace ProcessManagement.Services
+{
+    public static class KHOServices
+    {
+
+    }
+}
