80061c3c684d5a5c5632fcdd21f81a3bf9d2f99d
Ton Tan Khang <87051539+KhangToon@users.noreply.github.com>
2025-03-21 15:42:47 +0700

    # Extend ThungTP properties

--- ProcessManagement/Models/KHSXs/ThungTPham.cs
+++ ProcessManagement/Models/KHSXs/ThungTPham.cs
@@ -8,6 +8,7 @@ namespace ProcessManagement.Models.KHSXs
         public Propertyy TTPID { get; set; } = new() { DBName = DBName.TTPID, DisplayName = DispName.TTPID, Type = typeof(int), AlowDatabase = false, AlowDisplay = false, DispDatagrid = false }; // Identity ID
         public Propertyy KHSXID { get; set; } = new() { DBName = DBName.KHSXID, DisplayName = DispName.KHSXID, Type = typeof(int), AlowDatabase = true };
         public Propertyy VTofTPID { get; set; } = new() { DBName = DBName.VTofTPID, DisplayName = DispName.VTofTPID, Type = typeof(int), AlowDatabase = true };
+        public Propertyy VTTPID { get; set; } = new() { DBName = DBName.VTTPID, DisplayName = DispName.VTTPID, Type = typeof(int), AlowDatabase = true };
         public Propertyy MaQuanLyLot { get; set; } = new() { DBName = DBName.MaQuanLyLot, DisplayName = DispName.MaQuanLyLot, Type = typeof(string), AlowDatabase = true };
         public Propertyy MaQuanLyThung { get; set; } = new() { DBName = DBName.MaQuanLyThung, DisplayName = DispName.MaQuanLyThung, Type = typeof(string), AlowDatabase = true };
         public Propertyy IDThung { get; set; } = new() { DBName = DBName.IDThung, DisplayName = DispName.IDThung, Type = typeof(string), AlowDatabase = true };
@@ -15,16 +16,31 @@ namespace ProcessManagement.Models.KHSXs
         public Propertyy SLRequired { get; set; } = new() { DBName = DBName.SLRequired, DisplayName = DispName.SLRequired, Type = typeof(int), AlowDatabase = true };
         public Propertyy NgayDongThung { get; set; } = new() { DBName = DBName.NgayDongThung, DisplayName = DispName.NgayDongThung, Type = typeof(DateTime), AlowDatabase = true };
         public Propertyy NgayNhapKho { get; set; } = new() { DBName = DBName.NgayNhapKho, DisplayName = DispName.NgayNhapKho, Type = typeof(DateTime), AlowDatabase = true };
+        public Propertyy NgayXuatKho { get; set; } = new() { DBName = DBName.NgayXuatKho, DisplayName = DispName.NgayXuatKho, Type = typeof(DateTime), AlowDatabase = true };
         public Propertyy SPID { get; set; } = new() { DBName = DBName.SPID, DisplayName = DispName.SPID, Type = typeof(int), AlowDatabase = true };
+        public Propertyy PNKTPID { get; set; } = new() { DBName = DBName.PNKTPID, DisplayName = DispName.PNKTPID, Type = typeof(int), AlowDatabase = true };
+        public Propertyy PXKTPID { get; set; } = new() { DBName = DBName.PXKTPID, DisplayName = DispName.PXKTPID, Type = typeof(int), AlowDatabase = true };
+        public Propertyy PalletKey { get; set; } = new() { DBName = DBName.PalletKey, DisplayName = DispName.PalletKey, Type = typeof(string), AlowDatabase = true };
+        public Propertyy InStock { get; set; } = new() { DBName = DBName.InStock, DisplayName = DispName.InStock, Type = typeof(int), AlowDatabase = true };
 
+        public bool DaNhapKho { get; set; } = false;
+        public bool IsExpand { get; set; } = false;
+        public bool IsSelected { get; set; } = false;
+        public object? OriginSoLuong { get; set; }
+
+        public bool IsScanned { get; set; } = false;
 
         public static class DBName
         {
-            public const string Table_ThungTPham = "KHSX_ThungTPham";
+            public const string Table_KhoThungTPham = "KHOTP_ThungTPham";
             public const string TTPID = "TTPID";
             public const string KHSXID = "KHSXID";
             public const string SPID = "SPID";
+            public const string PNKTPID = "PNKTPID";
+            public const string PXKTPID = "PXKTPID";
+            public const string PalletKey = "PalletKey";
             public const string VTofTPID = "VTofTPID";
+            public const string VTTPID = "VTTPID";
             public const string MaQuanLyLot = "maquanlylot";
             public const string MaQuanLyThung = "maquanlythungtp";
             public const string IDThung = "IDThung";
@@ -32,6 +48,8 @@ namespace ProcessManagement.Models.KHSXs
             public const string SLRequired = "slrequired";
             public const string NgayDongThung = "ngaydongthung";
             public const string NgayNhapKho = "ngaynhapkho";
+            public const string NgayXuatKho = "ngayxuatkho";
+            public const string InStock = "InStock";
         }
 
         public static class DispName
@@ -39,7 +57,11 @@ namespace ProcessManagement.Models.KHSXs
             public const string TTPID = "TTPID";
             public const string KHSXID = "KHSXID";
             public const string SPID = "SPID";
+            public const string PNKTPID = "PNKTPID";
+            public const string PXKTPID = "PXKTPID";
+            public const string PalletKey = "PalletKey";
             public const string VTofTPID = "VTofTPID";
+            public const string VTTPID = "VTTPID";
             public const string MaQuanLyLot = "Mã quản lý LOT";
             public const string MaQuanLyThung = "Mã quản lý thùng";
             public const string IDThung = "IDThung";
@@ -47,6 +69,8 @@ namespace ProcessManagement.Models.KHSXs
             public const string SLRequired = "Số lượng đóng thùng";
             public const string NgayDongThung = "Ngày đóng thùng";
             public const string NgayNhapKho = "Ngày nhập kho";
+            public const string NgayXuatKho = "Ngày xuất kho";
+            public const string InStock = "In Stock";
         }
 
         // Get all property of this class
