6c3d655986c9d11620a8ef59ecd457407381eb27
Ton Tan Khang <87051539+KhangToon@users.noreply.github.com>
2025-03-25 17:33:02 +0700

    # Add ChartTracking Page

---
+++ ProcessManagement/Pages/KehoachSX/BieuDo/Models/ChartItem.cs
@@ -0,0 +1,8 @@
+﻿namespace ProcessManagement.Pages.KehoachSX.BieuDo.Models
+{
+    public class ChartItem
+    {
+        public string Property { get; set; } = string.Empty;
+        public int Value { get; set; }
+    }
+}
