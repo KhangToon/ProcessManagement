870ec13e290697247c4f99df2e4399a14e70d866
Ton Tan Khang <87051539+KhangToon@users.noreply.github.com>
2025-04-02 10:14:22 +0700

    # Modify Chart

---
+++ ProcessManagement/Commons/ColorBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+
+namespace ProcessManagement.Commons
+{
+    public static class ColorBuilder
+    {
+        public static Color[] CategoricalTwelveColors = new Color[]
+        {
+            Color.Red,
+            Color.Green,
+            Color.Blue,
+            Color.Yellow,
+            Color.Cyan,
+            Color.Magenta,
+            Color.Orange,
+            Color.Purple,
+            Color.Brown,
+            Color.Gray,
+            Color.Pink,
+            Color.Teal
+        };
+
+        public static Color ToColor(this Color color)
+        {
+            // Implement conversion logic if needed
+            return color;
+        }
+
+        public static string ToRgbString(this Color color)
+        {
+            return $"rgb({color.R}, {color.G}, {color.B})";
+        }
+    }
+}
