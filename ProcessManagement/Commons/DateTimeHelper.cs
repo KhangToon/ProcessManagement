a0d2ee9e7e1721268a241cbed5c4f2ef21fe2b0f
Ton Tan Khang <87051539+KhangToon@users.noreply.github.com>
2025-05-21 10:36:11 +0700

    # Add DatetimeHelper class for convert/extract with SQLServer Datetime

---
+++ ProcessManagement/Commons/DateTimeHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+
+namespace ProcessManagement.Commons
+{
+    public static class DateTimeHelper
+    {
+        private const string SQLDateFormat = "yyyy-MM-dd HH:mm:ss"; // Base format for SQL without milliseconds
+
+        public static DateTime? ExTractFromSQLDateFormat(object? inputSQLDate)
+        {
+            if (inputSQLDate == null)
+            {
+                return null; // Return null if inputSQLDate is null
+            }
+
+            string? dateString = inputSQLDate.ToString();
+            if (string.IsNullOrWhiteSpace(dateString))
+            {
+                return null; // Return null if the string representation is empty or whitespace
+            }
+
+            // Define the possible formats
+            string[] formats = {
+                        "yyyy-MM-dd HH:mm:ss",      // Format without milliseconds
+                        "yyyy-MM-dd HH:mm:ss.fff"    // Format with milliseconds
+            };
+
+            // Try parsing the date
+            if (DateTime.TryParseExact(dateString, formats, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime result))
+            {
+                return result; // Return the parsed DateTime
+            }
+
+            return null; // Return null if parsing fails
+        }
+
+        public static DateTime? ConvertToSQLDatetimeFormat(DateTime inputDate)
+        {
+            string formattedDate = inputDate.ToString(SQLDateFormat, CultureInfo.InvariantCulture);
+            return DateTime.ParseExact(formattedDate, SQLDateFormat, CultureInfo.InvariantCulture);
+        }
+
+    }
+}
